1c9ff078b03a30070b4158834f8dbe40c8b6cdda
shubhamtaliyan <128593428+shubhamtaliyan@users.noreply.github.com>
2023-03-22 16:12:16 +0530

    Add files via upload

---
+++ CelToFah.java
@@ -0,0 +1,9 @@
+class CelToFah
+	{
+		public static void main(String args[]) {
+		float cel=40.0f;
+		float fah;
+		fah=(cel*9/5)+32.0f;
+		System.out.println("The Temp in farhenite is "+fah);
+		}
+    }
