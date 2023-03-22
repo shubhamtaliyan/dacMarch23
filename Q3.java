25182b2344b27c7a756da47e81132db34d8768e3
shubhamtaliyan <128593428+shubhamtaliyan@users.noreply.github.com>
2023-03-22 16:17:53 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,17 @@
+class Q3
+{
+	public static void main(String args[])
+	{
+		
+		for(int i=1;i<=5;i++)
+		{
+			int a=65;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print((char)(a));
+				a++;
+			}
+			System.out.println();
+		}
+	}
+}
